988bf45777b54bbf9151b8c8c925558c6405bb4e
Yasuhiro SHIMIZU <the.phantom.bane@gmail.com>
2022-09-30 00:24:32 +0900

    docs: add some comments to EventForegroundTask (#12)

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/scheduler/EventForegroundTask.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/scheduler/EventForegroundTask.kt
@@ -7,6 +7,14 @@ import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
+/**
+ * Events are sent to the server in the following two cases:
+ *
+ * 1. New event is added to the cache, and the cache exceeded the limit.
+ * 2. Periodic flushing job.
+ *
+ * In the first case, we need to [reschedule] periodic flushing job to reduce unnecessary execution.
+ */
 internal class EventForegroundTask(
   private val component: Component,
   private val executor: ScheduledExecutorService,
@@ -14,18 +22,22 @@ internal class EventForegroundTask(
 
   private var scheduledFuture: ScheduledFuture<*>? = null
 
+  // This listener is called whenever the event cache is updated
   private val eventUpdateListener = EventInteractor.EventUpdateListener { _ ->
     executor.execute {
+      // send events if the cache exceeded the limit
       val result = component.eventInteractor.sendEvents(force = false)
       if (result is SendEventsResult.Success && result.sent) {
+        // reschedule the background task if event is actually sent.
         reschedule()
       }
     }
   }
 
-  internal fun reschedule() {
+  private fun reschedule() {
     scheduledFuture?.cancel(false)
     scheduledFuture = executor.scheduleWithFixedDelay(
+      // background task should flush(force-send) events
       { component.eventInteractor.sendEvents(force = true) },
       component.config.eventsFlushInterval,
       component.config.eventsFlushInterval,
