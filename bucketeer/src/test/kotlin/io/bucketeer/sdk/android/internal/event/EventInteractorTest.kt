123f909a6e9da34b89e72d23337064c20206ed4d
kenji <duyhung71089@gmail.com>
2023-06-02 11:30:02 +0700

    fix: lint fail

--- bucketeer/src/test/kotlin/io/bucketeer/sdk/android/internal/event/EventInteractorTest.kt
+++ bucketeer/src/test/kotlin/io/bucketeer/sdk/android/internal/event/EventInteractorTest.kt
@@ -719,7 +719,7 @@ class EventInteractorTest {
         .setBody(
           moshi.adapter(RegisterEventsResponse::class.java).toJson(
             RegisterEventsResponse(
-              errors = mapOf( 
+              errors = mapOf(
                 idGenerator.calls[0] to RegisterEventsErrorResponse(retriable = true, "error"),
                 idGenerator.calls[1] to RegisterEventsErrorResponse(retriable = false, "error"),
               ),
