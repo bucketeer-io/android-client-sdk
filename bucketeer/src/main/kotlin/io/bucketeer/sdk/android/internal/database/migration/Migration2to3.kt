6ba441f8bb741871fce8f3185bdd1786095ddfaf
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2023-07-12 19:09:16 +0900

    refactor: add comment

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/database/migration/Migration2to3.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/database/migration/Migration2to3.kt
@@ -9,6 +9,9 @@ import io.bucketeer.sdk.android.internal.event.EventEntity
 class Migration2to3 : Migration {
 
   override fun migrate(db: SupportSQLiteDatabase, sharedPreferences: SharedPreferences) {
+    // Due to changes in the API endpoint, we must delete all the data stored in the SQLite because
+    // the data model has changed from snake case to camel case.
+    // Reference: https://github.com/bucketeer-io/android-client-sdk/pull/63
     db.delete(EvaluationEntity.TABLE_NAME, null, null)
     db.delete(EventEntity.TABLE_NAME, null, null)
 
