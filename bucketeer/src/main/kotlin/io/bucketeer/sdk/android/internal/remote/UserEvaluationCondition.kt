46ea49b07e17e65513beefee79653bc5556407c7
kenji <duyhung71089@gmail.com>
2023-09-13 13:49:04 +0700

    fix: json parser for user_evaluation_condition fail

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/remote/UserEvaluationCondition.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/remote/UserEvaluationCondition.kt
@@ -1,7 +1,8 @@
 package io.bucketeer.sdk.android.internal.remote
 
-import java.util.jar.Attributes
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class UserEvaluationCondition(
   val evaluatedAt: String,
   val userAttributesUpdated: String,
