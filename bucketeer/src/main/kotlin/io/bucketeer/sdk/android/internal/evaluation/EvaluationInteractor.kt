62a4c6efeb2c882e220cedf74613f5d12f6bba6b
kenji <duyhung71089@gmail.com>
2023-10-01 16:38:16 +0700

    chore: update comments

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/evaluation/EvaluationInteractor.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/evaluation/EvaluationInteractor.kt
@@ -89,6 +89,9 @@ internal class EvaluationInteractor(
             // 2- Save the UserEvaluations.CreatedAt in the response as evaluatedAt in the SharedPreferences
             evaluationStorage.deleteAllAndInsert(currentEvaluations, newEvaluatedAt)
           } else {
+            // 1- Check the evaluation list in the response and upsert them in the DB if the list is not empty
+            // 2- Check the list of the feature flags that were archived on the console and delete them from the DB
+            // 3- Save the UserEvaluations.CreatedAt in the response as evaluatedAt in the SharedPreferences
             val archivedFeatureIds = response.evaluations.archivedFeatureIds
             val updatedEvaluations = response.evaluations.evaluations
             shouldNotifyListener =
