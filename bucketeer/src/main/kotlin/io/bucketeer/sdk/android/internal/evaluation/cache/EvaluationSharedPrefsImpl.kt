93107eb8864e1f4e0c1fcf477b690bc414c37bad
duyhungtnn <duyhung71089@gmail.com>
2023-10-02 14:33:33 +0700

    Update bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/evaluation/cache/EvaluationSharedPrefsImpl.kt
    format code
    
    Co-authored-by: Yasuhiro SHIMIZU <the.phantom.bane@gmail.com>

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/evaluation/cache/EvaluationSharedPrefsImpl.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/evaluation/cache/EvaluationSharedPrefsImpl.kt
@@ -4,8 +4,9 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import io.bucketeer.sdk.android.internal.Constants
 
-internal class EvaluationSharedPrefsImpl(private val sharedPrefs: SharedPreferences) :
-  EvaluationSharedPrefs {
+internal class EvaluationSharedPrefsImpl(
+  private val sharedPrefs: SharedPreferences
+) : EvaluationSharedPrefs {
   override var currentEvaluationsId: String
     get() = sharedPrefs.getString(Constants.PREFERENCE_KEY_USER_EVALUATION_ID, "") ?: ""
 
