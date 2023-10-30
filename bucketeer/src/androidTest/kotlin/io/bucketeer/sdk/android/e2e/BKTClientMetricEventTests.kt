056cb1777a69e427cc13aefe04ebd98afd422754
kenji <duyhung71089@gmail.com>
2023-10-30 14:19:11 +0700

    chore: init metrics event tests

---
+++ bucketeer/src/androidTest/kotlin/io/bucketeer/sdk/android/e2e/BKTClientMetricEventTests.kt
@@ -0,0 +1,54 @@
+package io.bucketeer.sdk.android.e2e
+
+import android.content.Context
+import androidx.test.annotation.UiThreadTest
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import io.bucketeer.sdk.android.BKTClient
+import io.bucketeer.sdk.android.BKTConfig
+import io.bucketeer.sdk.android.BKTUser
+import io.bucketeer.sdk.android.BuildConfig
+import io.bucketeer.sdk.android.internal.Constants
+import io.bucketeer.sdk.android.internal.database.OpenHelperCallback
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BKTClientMetricEventTests {
+  private lateinit var context: Context
+  private lateinit var config: BKTConfig
+  private lateinit var user: BKTUser
+
+  @Before
+  @UiThreadTest
+  fun setup() {
+    context = ApplicationProvider.getApplicationContext()
+  }
+
+  @After
+  @UiThreadTest
+  fun tearDown() {
+    BKTClient.destroy()
+    context.deleteDatabase(OpenHelperCallback.FILE_NAME)
+    context.getSharedPreferences(Constants.PREFERENCES_NAME, Context.MODE_PRIVATE)
+      .edit()
+      .clear()
+      .commit()
+  }
+
+  @Test
+  fun testUsingRandomStringInTheAPIKeyShouldThrowForbidden() {
+    config = BKTConfig.builder()
+      .apiKey("random_key")
+      .apiEndpoint(BuildConfig.API_ENDPOINT)
+      .featureTag(FEATURE_TAG)
+      .appVersion("1.2.3")
+      .build()
+
+    val result = BKTClient.initialize(context, config, user).get()
+    Truth.assertThat(result).isNull()
+  }
+}
