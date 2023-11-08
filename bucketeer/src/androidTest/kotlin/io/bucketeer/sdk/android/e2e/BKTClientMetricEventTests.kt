f325c12f415759af9de5468285426fd90ef0597f
kenji <duyhung71089@gmail.com>
2023-11-08 22:40:51 +0700

    chore: add e2e metrics test

--- bucketeer/src/androidTest/kotlin/io/bucketeer/sdk/android/e2e/BKTClientMetricEventTests.kt
+++ bucketeer/src/androidTest/kotlin/io/bucketeer/sdk/android/e2e/BKTClientMetricEventTests.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import androidx.test.annotation.UiThreadTest
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import io.bucketeer.sdk.android.BKTClient
+import io.bucketeer.sdk.android.BKTClientImpl
 import io.bucketeer.sdk.android.BKTConfig
+import io.bucketeer.sdk.android.BKTException
 import io.bucketeer.sdk.android.BKTUser
 import io.bucketeer.sdk.android.BuildConfig
 import io.bucketeer.sdk.android.internal.Constants
 import io.bucketeer.sdk.android.internal.database.OpenHelperCallback
+import io.bucketeer.sdk.android.internal.di.ComponentImpl
+import io.bucketeer.sdk.android.internal.model.ApiId
+import io.bucketeer.sdk.android.internal.model.EventData
+import io.bucketeer.sdk.android.internal.model.EventType
+import io.bucketeer.sdk.android.internal.model.MetricsEventType
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -26,6 +33,9 @@ class BKTClientMetricEventTests {
   @UiThreadTest
   fun setup() {
     context = ApplicationProvider.getApplicationContext()
+    user = BKTUser.builder()
+      .id(USER_ID)
+      .build()
   }
 
   @After
@@ -42,6 +52,7 @@ class BKTClientMetricEventTests {
   // Metrics Event Tests
   // refs: https://github.com/bucketeer-io/javascript-client-sdk/blob/main/e2e/events.spec.ts#L112
   @Test
+  @UiThreadTest
   fun testUsingRandomStringInTheAPIKeyShouldThrowForbidden() {
     config = BKTConfig.builder()
       .apiKey("random_key")
@@ -51,12 +62,79 @@ class BKTClientMetricEventTests {
       .build()
 
     val result = BKTClient.initialize(context, config, user).get()
-    Truth.assertThat(result).isNull()
+    // Using a random string in the api key setting should throw Forbidden
+    assertThat(result).isInstanceOf(BKTException.ForbiddenException::class.java)
+
+    val client = BKTClient.getInstance() as BKTClientImpl
+    val eventDao = (client.component as ComponentImpl).dataModule.eventSQLDao
+
+    Thread.sleep(100)
+    val events = eventDao.getEvents()
+    assertThat(events.count()).isEqualTo(1)
+    assertThat(
+      events.any {
+        val type = it.type
+        val event = it.event
+        return@any type == EventType.METRICS &&
+            event is EventData.MetricsEvent &&
+            event.type == MetricsEventType.FORBIDDEN_ERROR &&
+            event.event.apiId == ApiId.GET_EVALUATIONS
+      },
+    ).isTrue()
+
+    val flushResult = client.flush().get()
+    // Using a random string in the api key setting should throw Forbidden
+    assertThat(flushResult).isInstanceOf(BKTException.ForbiddenException::class.java)
+    assertThat(eventDao.getEvents().count()).isEqualTo(2)
   }
 
   @Test
-  fun testARandomStringInTheFeatureTagShouldNotAffectAPIRequest() {}
+  @UiThreadTest
+  fun testARandomStringInTheFeatureTagShouldNotAffectAPIRequest() {
+    config = BKTConfig.builder()
+      .apiKey(BuildConfig.API_KEY)
+      .apiEndpoint(BuildConfig.API_ENDPOINT)
+      .featureTag("random-string-abc")
+      .appVersion("1.2.3")
+      .build()
+
+    val result = BKTClient.initialize(context, config, user).get()
+    assertThat(result).isNull()
+  }
 
   @Test
-  fun testTimeout() {}
+  @UiThreadTest
+  fun testTimeout() {
+    config = BKTConfig.builder()
+      .apiKey(BuildConfig.API_KEY)
+      .apiEndpoint(BuildConfig.API_ENDPOINT)
+      .featureTag("random-string-abc")
+      .appVersion("1.2.3")
+      .build()
+
+    val result = BKTClient.initialize(context, config, user, timeoutMillis = 10).get()
+    assertThat(result).isInstanceOf(BKTException.TimeoutException::class.java)
+
+    val client = BKTClient.getInstance() as BKTClientImpl
+    val eventDao = (client.component as ComponentImpl).dataModule.eventSQLDao
+
+    Thread.sleep(100)
+    val events = eventDao.getEvents()
+    assertThat(events.count()).isEqualTo(1)
+    assertThat(
+      events.any {
+        val type = it.type
+        val event = it.event
+        return@any type == EventType.METRICS &&
+            event is EventData.MetricsEvent &&
+            event.type == MetricsEventType.TIMEOUT_ERROR &&
+            event.event.apiId == ApiId.GET_EVALUATIONS
+      },
+    ).isTrue()
+
+    val flushResult = client.flush().get()
+    // Using a random string in the api key setting should throw Forbidden
+    assertThat(flushResult).isNull()
+    assertThat(eventDao.getEvents().count()).isEqualTo(0)
+  }
 }
