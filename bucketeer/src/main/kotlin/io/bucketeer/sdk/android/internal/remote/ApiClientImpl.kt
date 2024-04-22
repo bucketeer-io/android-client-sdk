9733236a958be7c9fcf26e46f9c88d0a49124d5f
duyhungtnn <duyhung71089@gmail.com>
2024-04-22 13:53:00 +0700

    chore: remove unnecessary code

--- bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/remote/ApiClientImpl.kt
+++ bucketeer/src/main/kotlin/io/bucketeer/sdk/android/internal/remote/ApiClientImpl.kt
@@ -4,7 +4,6 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import io.bucketeer.sdk.android.internal.logd
 import io.bucketeer.sdk.android.internal.model.Event
-import io.bucketeer.sdk.android.internal.model.SourceID
 import io.bucketeer.sdk.android.internal.model.User
 import io.bucketeer.sdk.android.internal.model.request.GetEvaluationsRequest
 import io.bucketeer.sdk.android.internal.model.request.RegisterEventsRequest
@@ -120,7 +119,7 @@ internal class ApiClientImpl(
   }
 
   override fun registerEvents(events: List<Event>): RegisterEventsResult {
-    val body = RegisterEventsRequest(events = events, sourceId = SourceID.ANDROID)
+    val body = RegisterEventsRequest(events = events)
 
     val request = Request.Builder()
       .url(
