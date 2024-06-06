3ce515d4a09bfcf635b3cd400cf3c971b374d9cf
duyhungtnn <duyhung71089@gmail.com>
2024-06-06 11:03:05 +0700

    fix: lint fail

--- bucketeer/src/test/kotlin/io/bucketeer/sdk/android/LoggerHolderTest.kt
+++ bucketeer/src/test/kotlin/io/bucketeer/sdk/android/LoggerHolderTest.kt
@@ -9,44 +9,49 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 internal class LoggerHolderTest {
-
   @Test
-  fun testConcurrentModification(): Unit = runBlocking {
-    class TestLogger : BKTLogger {
-      override fun log(priority: Int, messageCreator: (() -> String?)?, throwable: Throwable?) {
-
-      }
-    }
-
-    // Add initial logger
-    LoggerHolder.addLogger(TestLogger())
-
-    // Launch concurrent coroutines to add loggers and log messages
-    val job1 = launch(Dispatchers.Default) {
-      repeat(1000) {
-        try {
-          logd { "Test message"}
-          LoggerHolder.addLogger(TestLogger())
-          logd { "Test message"}
-        } catch (e: ConcurrentModificationException) {
-          fail("ConcurrentModificationException occurred")
+  fun testConcurrentModification(): Unit =
+    runBlocking {
+      class TestLogger : BKTLogger {
+        override fun log(
+          priority: Int,
+          messageCreator: (() -> String?)?,
+          throwable: Throwable?,
+        ) {
         }
       }
-    }
 
-    val job2 = launch(Dispatchers.IO) {
-      repeat(1000) {
-        try {
-          logd { "Test message"}
-          LoggerHolder.addLogger(TestLogger())
-          LoggerHolder.addLogger(TestLogger())
-        } catch (e: ConcurrentModificationException) {
-          fail("ConcurrentModificationException occurred")
+      // Add initial logger
+      LoggerHolder.addLogger(TestLogger())
+
+      // Launch concurrent coroutines to add loggers and log messages
+      val job1 =
+        launch(Dispatchers.Default) {
+          repeat(1000) {
+            try {
+              logd { "Test message" }
+              LoggerHolder.addLogger(TestLogger())
+              logd { "Test message" }
+            } catch (e: ConcurrentModificationException) {
+              fail("ConcurrentModificationException occurred")
+            }
+          }
         }
-      }
-    }
 
-    // Wait for both coroutines to finish
-    joinAll(job1, job2)
-  }
+      val job2 =
+        launch(Dispatchers.IO) {
+          repeat(1000) {
+            try {
+              logd { "Test message" }
+              LoggerHolder.addLogger(TestLogger())
+              LoggerHolder.addLogger(TestLogger())
+            } catch (e: ConcurrentModificationException) {
+              fail("ConcurrentModificationException occurred")
+            }
+          }
+        }
+
+      // Wait for both coroutines to finish
+      joinAll(job1, job2)
+    }
 }
